c6f3f34f84aafe0584c29b15893bbc37d91fae8a
Megaxuze <112304241+Megaxuze@users.noreply.github.com>
2024-09-23 15:31:27 +0000

    Update moonbix.sh

--- moonbix.sh
+++ moonbix.sh
@@ -20,8 +20,10 @@ chmod +x /usr/bin/moonbix
 apt update
 apt install python3-pip python3 nano git screen -y
 
+apt-get install screen
+
 cd /root
-git clone https://github.com/zamzasalim/moonbix.git
+git clone https://github.com/zamzasalim/ERROR.git
 
 cd /root/moonbix
 pip install -r requirements.txt --break-system-packages
@@ -29,31 +31,3 @@ pip install -r requirements.txt --break-system-packages
 [[ -f /root/moonbix.sh ]] && rm /root/moonbix.sh
 [[ -f requirements.txt ]] && rm requirements.txt
 
-while true; do
-    read -p "Masukkan token baru (atau ketik 'exit' untuk selesai): " token_input
-    if [[ "$token_input" == "exit" ]]; then
-        break
-    fi
-    echo "$token_input" >> tokens.txt
-done
-
-proxy_file="/root/moonbix/proxy.txt"
-read -p "Apakah Anda ingin menambahkan proxy? (y/n): " use_proxy
-
-if [[ "$use_proxy" == "y" || "$use_proxy" == "Y" ]]; then
-    while true; do
-        read -p "Masukkan proxy baru (atau ketik 'exit' untuk selesai): " proxy_input
-        if [[ "$proxy_input" == "exit" ]]; then
-            break
-        fi
-        echo "$proxy_input" >> "$proxy_file"
-    done
-fi
-
-if [[ -f "$proxy_file" && -s "$proxy_file" ]]; then
-    screen -S moonbix -dm bash -c "http_proxy=$(<"$proxy_file") https_proxy=$(<"$proxy_file") python3 main.py"
-else
-    screen -S moonbix -dm bash -c "python3 main.py"
-fi
-
-echo "Gunakan 'screen -r moonbix' untuk mengakses sesi."

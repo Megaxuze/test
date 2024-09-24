89909f7103edc12d6c5027c44b4e3499a6c940b5
Megaxuze <112304241+Megaxuze@users.noreply.github.com>
2024-09-24 15:06:31 +0700

    Add files via upload

---
+++ moonbix (1).sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+log() {
+    local level=$1
+    local message=$2
+    echo "[$level] $message"
+}
+print_green() {
+    echo -e "\e[32m$1\e[0m"
+}
+curl -s https://data.zamzasalim.xyz/file/uploads/asclogo.sh | bash
+sleep 5
+
+echo "MOONBIX BOT INSTALL"
+sleep 2
+
+echo "cd /root/moonbix && python3 main.py" > /usr/bin/moonbix
+chmod +x /usr/bin/moonbix
+
+apt update
+apt install python3-pip python3 nano git screen -y
+
+apt-get install screen
+
+cd /root
+git clone https://github.com/zamzasalim/moonbix-bot.git
+
+cd /root/moonbix-bot
+python3 -m pip install -r requirements.txt
+
+[[ -f /root/moonbix.sh ]] && rm /root/moonbix.sh
+[[ -f requirements.txt ]] && rm requirements.txt
+
+cd /root/moonbix-bot

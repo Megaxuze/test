f8898057f04174d32797603ff5fb908735c979b8
Megaxuze <112304241+Megaxuze@users.noreply.github.com>
2024-09-25 01:28:41 +0000

    Update notpixel.sh

--- notpixel.sh
+++ notpixel.sh
@@ -30,9 +30,3 @@ pip install pillow==10.4.0
 pip install setproctitle==1.3.3
 pip install requests==2.31.0
 pip install crayons==0.4.0
-
-[[ -f /root/notpixel.sh ]] && rm /root/notpixel.sh
-[[ -f requirements.txt ]] && rm requirements.txt
-
-cd /root/notpixel
-cd notpixel
